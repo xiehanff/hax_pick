88b066566cc48a13b0f21afdfd35ca818885102e
hax <xiehanff@gmail.com>
2026-09-04 09:33:57 +0800

    fix: render in-app brand icon from app bundle icon

--- Sources/AppBrandIcon.swift
+++ Sources/AppBrandIcon.swift
@@ -1,23 +1,14 @@
+import AppKit
 import SwiftUI
 
 struct AppBrandIcon: View {
     let size: CGFloat
 
     var body: some View {
-        if let image = iconImage {
-            Image(nsImage: image)
-                .resizable()
-                .interpolation(.high)
-                .frame(width: size, height: size)
-                .accessibilityHidden(true)
-        }
-    }
-
-    private var iconImage: NSImage? {
-        guard let path = Bundle.main.path(forResource: "MenuBarIcon", ofType: "png"),
-              let image = NSImage(contentsOfFile: path) else {
-            return nil
-        }
-        return image
+        Image(nsImage: NSApplication.shared.applicationIconImage)
+            .resizable()
+            .interpolation(.high)
+            .frame(width: size, height: size)
+            .accessibilityHidden(true)
     }
 }
