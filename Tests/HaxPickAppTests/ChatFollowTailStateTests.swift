718eb75d96fbbb651091481cccfc0a5f4c4feee8
hax <xiehanff@gmail.com>
2026-09-07 17:27:47 +0800

    test: cover manual return to streaming tail

--- Tests/HaxPickAppTests/ChatFollowTailStateTests.swift
+++ Tests/HaxPickAppTests/ChatFollowTailStateTests.swift
@@ -21,4 +21,38 @@ final class ChatFollowTailStateTests: XCTestCase {
         state.requestDidStart()
         XCTAssertTrue(state.isFollowingTail)
     }
+
+    func testReturningToViewportBottomRestoresFollowingAutomatically() {
+        var state = ChatFollowTailState()
+        state.userDidScroll()
+        XCTAssertFalse(state.isFollowingTail)
+
+        XCTAssertFalse(
+            state.tailPositionDidChange(
+                tailMaxY: 520,
+                viewportHeight: 420
+            )
+        )
+        XCTAssertFalse(state.isFollowingTail)
+
+        XCTAssertTrue(
+            state.tailPositionDidChange(
+                tailMaxY: 448,
+                viewportHeight: 420
+            )
+        )
+        XCTAssertTrue(state.isFollowingTail)
+    }
+
+    func testTailGeometryDoesNotChangeAnAlreadyFollowingState() {
+        var state = ChatFollowTailState()
+
+        XCTAssertFalse(
+            state.tailPositionDidChange(
+                tailMaxY: 410,
+                viewportHeight: 420
+            )
+        )
+        XCTAssertTrue(state.isFollowingTail)
+    }
 }
